15a433b8283ce656552ee0c2d2bfea58dca93085
shumaker100 <utahshu@gmail.com>
2021-02-11 09:34:49 -0700

    update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.listen(5000, () => {
-    console.log('server has started on port 5000')
+    console.log('server has started on port 5000');
 });
 
