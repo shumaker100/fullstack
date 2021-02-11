7c819ccc5f057041c1505c27289eac881ed37a48
shumaker100 <utahshu@gmail.com>
2021-02-11 10:56:07 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+//routes//
+
+//create todo
+app.post('/todos', async(req, res) => {
+    try {
+
+        console.log(req.body);
+
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
+
 
 app.listen(5000, () => {
     console.log('server has started on port 5000');
