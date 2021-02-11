66fda35211c0628394998ba43e543487acd89606
shumaker100 <utahshu@gmail.com>
2021-02-11 16:04:36 -0700

    update

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import InputTodo from "./components/InputTodo"
+
+function App() {
+  return (<Fragment>
+    <InputTodo/>
+  </Fragment>)
+  
+}
+
+export default App;
