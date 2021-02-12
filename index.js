a6e5e4be18752960ca64a493016bf503ea5c0dc0
shumaker100 <utahshu@gmail.com>
2021-02-12 15:24:23 -0700

    ready to deploy

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const path = require("path");
+const PORT = process.env.PORT || 5000; 
 
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //test code
 
@@ -75,7 +81,7 @@ app.delete("/todos/:id", async(req, res) => {
 
 
 
-app.listen(5000, () => {
-    console.log('server has started on port 5000');
+app.listen(PORT, () => {
+    console.log(`server has started on port ${PORT}`);
 });
 
